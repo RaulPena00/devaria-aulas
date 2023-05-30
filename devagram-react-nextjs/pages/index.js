9ecff7607514665d5857fcd35aefa5dc4d237e50
Raul Pena Pereira <raulppereira00@gmail.com>
2023-05-30 08:22:34 -0300

    Inicio da tela de login

--- devagram-react-nextjs/pages/index.js
+++ devagram-react-nextjs/pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Botao from '@/components/botao'
+import Avatar from '@/components/avatar'
+import { UploadImagem } from '@/components/uploadImagem'
+import { useState, useRef } from 'react'
 
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+
   return (
     <>
     <h1>Olá mundo!</h1>
+    <button onClick={() => referenciaInput?.current?.click()}>abrir seletor de arquivos</button>
+
+    <UploadImagem 
+    setImagem={setImagem} 
+    imagemPreview={imagem?.preview}
+    aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+    />
+
+  <Avatar/>
+
+    <div style={{width: 250}}>
+      <Botao texto={'Login'} cor='invertido' manipularClique={() => console.log('botao clicado')}/>
+    </div>
     </>
   )
 }
